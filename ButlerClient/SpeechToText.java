080876258187afabc3d1a008427697bb4268ca3e
Marcel Schneider <Marcel.Schneider1@googlemail.com>
2015-07-10 20:03:35 +0200

    - Programmteil "SpeechToText.java" aufgeräumt und kommentiert.

--- ButlerClient/SpeechToText.java
+++ ButlerClient/SpeechToText.java
@@ -1,40 +1,37 @@
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
+import java.io.*;
 import java.util.*;
 
 public class SpeechToText {
 	public static void main(String[] args) throws IOException{
+		//	Hashmap für das Erkennen und Entfernen von .ret Dateileichen
 		Map<String, Integer> returncount = new HashMap<String, Integer>();
 
-		//You can also create your own buffer using the getTargetDataLine() method.
 		while(true){
 			String rootpath = "/home/pi/butlerclient/Processing";
 
+			//	Suchen nach einer Sprachdatei (.speech)
 			File dir = new File(rootpath);
 			File[] files = dir.listFiles(new FilenameFilter() {
 				public boolean accept(File dir, String name) {
 					return name.toLowerCase().endsWith(".speech");
 				}
 			});
-
 			File speechfile = null;
-
 			if(files != null){
 				for (File file : files) {
 					speechfile = new File(file.getCanonicalPath());
 				}
 			}
 
+			//	Wenn für die Sprachdatei eine Bearbeitungsdatei der GoogleSpeechApi (.ret) gefunden wurden, wird der Zähler für diese Übersetzung erhöht.
 			String returnFile = speechfile == null ? "" : speechfile.getCanonicalPath().replaceAll(".speech", ".ret");
-
 			int rc = returncount.containsKey(returnFile) ? returncount.get(returnFile) : 0;
 			rc++;
 			returncount.put(returnFile, rc);
 
+			//	Wenn die 60. Erkennung der Übersetzungsdatei gefunden wurde ...
 			if(rc > 60){
+				//	... wird diese als GoogleSpeechApi Leiche aus dem Dateisystem und der HashMap entfernt.
 				File retfile = new File(returnFile);
 				if(retfile.exists()){
 					retfile.delete();
@@ -42,19 +39,20 @@ public class SpeechToText {
 				}
 			}
 
+			//	Wenn eine Sprachdatei gefunden wurde, die noch keine Verarbeitungsdatei der GoogleSpeechApi hat ...
 			if(speechfile != null && speechfile.exists() && !new File(returnFile).exists()){
+				//	wird für die Übersetzung über die GoogleApi das Bashscript "SpeechToText.sh" gestartet.
 				ProcessBuilder process = new ProcessBuilder(
-					rootpath + "/SpeechToText.sh",
+					String.format("%1/SpeechToText.sh", rootpath),
 					speechfile.getName().replaceAll(".speech", "")
 				);
-
 				try{
 				  Process runningProcess = process.start();
 				  runningProcess.waitFor();
 				}
 				catch(Exception ex){}
 			}
-
+			
 			try{
 				Thread.sleep(500);
 			}
--- ButlerClient/compile.sh
+++ ButlerClient/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 javac VoiceDetector.java
 javac -cp jmf.jar:JavaGoogleText.jar:JavaFlacEncoder.jar:. Listener.java
-javac -cp jmf.jar:JavaGoogleText.jar:JavaFlacEncoder.jar:. SpeechToText.java
+javac SpeechToText.java
 javac -cp jmf.jar:JavaGoogleText.jar:JavaFlacEncoder.jar:. QuestionTeller.java
 javac -cp jmf.jar:JavaGoogleText.jar:JavaFlacEncoder.jar:. Babbler.java
